62921b07dcaa7880954cb51ce0ba497630faabc5
Hanna Khanko <ahanko802@gmail.com>
2022-01-30 16:20:57 +0300

    feat: Solution for JS: Brackets

--- src/index.js
+++ src/index.js
@@ -1,61 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-	    
-  let stack = [];
-
-
-  let lenstr = str.length;
-  if (lenstr % 2 != 0) return false;
-
-
-  let lenConfig = bracketsConfig.length;
-  let checkeven = 1;
-  let cheven;
-  let chClose;
-  for (let i = 0; i < lenstr; i++) {
-      let ch = str.charAt(i);
-      let close = false;
-      let open = false;
-      let even = false;
-      for (let j = 0; j < lenConfig; j++) {
-          if (bracketsConfig[j][0] == bracketsConfig[j][1] && ch == bracketsConfig[j][0]) {
-              even = true;
-              checkeven = (-1) * checkeven;
-              cheven = ch;
-              console.log('checkeven = ', checkeven)
-              break;
+        let pairs = bracketsConfig.map((e) => e.join(""));
+      
+        for (let i = 0; i < pairs.length; i++) {
+          if( str.includes(pairs[i])) {
+            str = str.replace(pairs[i], "");
+           i = -1;
           }
-          if (ch == bracketsConfig[j][0]) {
-              open = true;
-              break;
-          }
-          if (ch == bracketsConfig[j][1]) {
-              close = true;
-              chClose = bracketsConfig[j][0];
-              break;
-          }
-      }
-      if (even == true) {
-          if (checkeven == -1) {
-              stack.push(ch);
-              console.log('push=', ch);
-          }
-          if (checkeven == 1 && cheven == stack[stack.length - 1]) {
-              console.log(' pop stack[stack.length-1]=', stack[stack.length - 1], ', ch= ', ch)
-              stack.pop();
-          }
-      } else {
-
-
-          if (open === true) {
-              stack.push(ch);
-              console.log('push=', ch);
-          }
-          if (close === true && chClose == stack[stack.length - 1]) {
-              console.log(' pop stack[stack.length-1]=', stack[stack.length - 1], ', ch= ', ch)
-              stack.pop();
-          }
-      }
+        }
+    return str.length == 0;
   }
-  return (stack.length === 0);
-};
 
